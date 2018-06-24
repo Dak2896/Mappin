cdc4d261eab5c674cb097ef7a5794a999edcdc0d
Daniele Keller <danielealessandrokeller@ied.edu>
2018-06-24 14:23:40 +0200

    kernel function

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,7 +4,8 @@ namespace Map\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-
+use Map\Event;
+use Carbon\Carbon;
 class Kernel extends ConsoleKernel
 {
     /**
@@ -13,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+       \App\Console\Commands\Inspire::class,
     ];
 
     /**
@@ -24,8 +25,26 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+      $schedule->call(function () {
+        $events = Event::all()->pluck('end_date');
+        $now = Carbon::now();
+        if (is_null($events)) {
+            return $this->sendError('Events not set.');
+        }
+        foreach ($events as $eve)
+        {
+          $datework = new Carbon($eve);
+          $diff = $now->diffInMinutes($datework, false);
+          if($diff <= 0)
+          {
+            $event = Event::where('end_date', $eve)->update(['is_active'=> 0]);
+          }
+          if($diff > 1)
+          {
+            $event = Event::where('end_date', $eve)->update(['is_active'=> 1]);
+          }
+        }
+      })->everyMinute();
     }
 
     /**
