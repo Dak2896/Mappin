2b0774939249b237afd1ab0c36bf0dedbf5a3c96
Giona De Bernardi <>
2018-06-25 01:22:09 +0200

    diosssssss4

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,12 +30,10 @@ class HomeController extends Controller
     public function index()
     {
 
-<<<<<<< HEAD
-        $id = Auth::user()->admin;
-=======
+
         $id = Auth::user()->id;
         $admin = Auth::user()->admin;
->>>>>>> f39356f10328a305840d05f810617e4981085be4
+
         $names = Event::pluck('description');
         $chart = Charts::database(User::all(), 'bar', 'highcharts')
                         ->title('Sono io')
--- routes/api.php
+++ routes/api.php
@@ -22,35 +22,36 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 //Route::get('events/{event}', 'EventController@show');
 Route::resource('events', 'Api\EventApiController')->middleware('auth:api');
 
-
-
-
-Route::get('points/{point}', 'PointController@show');
-Route::resource('events', 'Api\EventApiController');
-Route::get('activePoints', 'Api\PointApiController@getActivePoints');
-Route::post('setActiveEvents', 'Api\EventApiController@setActiveEvents');
-Route::get('myEvents/{event_id}', 'Api\EventApiController@indexUser');
-Route::get('activeEvents/{user_id}', 'Api\EventApiController@aviableEvents');
-Route::get('getName/{user_id}', 'Api\MessageApiController@getName');
-
-Route::post('login', 'Api\PassportController@login');
-Route::post('register', 'Api\PassportController@register');
-Route::resource('messages', 'Api\MessageApiController');
-Route::get('messagesOfChat/{chat_id}', 'Api\MessageApiController@messageOfChat');
-
 Route::get('nameOfEvent/{user_id}','Api\ChatApiController@nameOfEvent');
 Route::get('activeChat/{user_id}','Api\ChatApiController@activeChat');
 
-Route::get('userName/{user_id}','Api\MessageApiController@userName');
-Route::post('get-details', 'Api\PassportController@getDetails');
-//Route::get('points', 'PointController@index');
-Route::resource('chats', 'Api\ChatApiController');
-Route::resource('points', 'Api\PointApiController');
-Route::resource('user_events', 'Api\User_EventApiController');
-Route::get('user_events_find/{id}/{user_id}','Api\User_EventApiController@findParecipationsOfUser');
+Route::post('register', 'Api\PassportController@register');
 
 Route::group(['middleware' => 'auth:api'], function(){
 
+  Route::get('points/{point}', 'PointController@show');
+  Route::resource('events', 'Api\EventApiController');
+  Route::get('activePoints', 'Api\PointApiController@getActivePoints');
+  Route::post('setActiveEvents', 'Api\EventApiController@setActiveEvents');
+  Route::get('myEvents/{event_id}', 'Api\EventApiController@indexUser');
+  Route::get('activeEvents/{user_id}', 'Api\EventApiController@aviableEvents');
+  Route::get('getName/{user_id}', 'Api\MessageApiController@getName');
+
+  Route::post('login', 'Api\PassportController@login');
+  Route::resource('messages', 'Api\MessageApiController');
+  Route::get('messagesOfChat/{chat_id}', 'Api\MessageApiController@messageOfChat');
+
+
+
+
+  Route::get('userName/{user_id}','Api\MessageApiController@userName');
+
+  Route::post('get-details', 'Api\PassportController@getDetails');
+  //Route::get('points', 'PointController@index');
+  Route::resource('chats', 'Api\ChatApiController');
+  Route::resource('points', 'Api\PointApiController');
+  Route::resource('user_events', 'Api\User_EventApiController');
+  Route::get('user_events_find/{id}/{user_id}','Api\User_EventApiController@findParecipationsOfUser');
 
 });
 
