4fd3ab03e77db4e47ca77f492e79fe6a7ed666b4
Daniele Keller <danielealessandrokeller@ied.edu>
2018-06-24 23:40:39 +0200

    schedule command

--- app/Console/Commands/setEvents.php
+++ app/Console/Commands/setEvents.php
@@ -10,25 +10,6 @@ class setEvents extends Command
 {
 
       protected $name = 'set:Events';
-      /*
-      $events = Event::all()->pluck('end_date');
-      $now = Carbon::now();
-      if (is_null($events)) {
-          //return $this->sendError('Events not set.');
-      }
-      foreach ($events as $eve)
-      {
-        $datework = new Carbon($eve);
-        $diff = $now->diffInMinutes($datework, false);
-        if($diff <= 0)
-        {
-          $event = Event::where('end_date', $eve)->update(['is_active'=> 0]);
-        }
-        if($diff > 1)
-        {
-          $event = Event::where('end_date', $eve)->update(['is_active'=> 1]);
-        }
-    }*/
 
 
     /**
@@ -55,6 +36,23 @@ class setEvents extends Command
      */
     public function handle()
     {
-        //
+      $events = Event::all()->pluck('end_date');
+      $now = Carbon::now();
+      if (is_null($events)) {
+          //return $this->sendError('Events not set.');
+      }
+      foreach ($events as $eve)
+      {
+        $datework = new Carbon($eve);
+        $diff = $now->diffInMinutes($datework, false);
+        if($diff <= 0)
+        {
+          $event = Event::where('end_date', $eve)->update(['is_active'=> 0]);
+        }
+        if($diff > 1)
+        {
+          $event = Event::where('end_date', $eve)->update(['is_active'=> 1]);
+        }
+    }
     }
 }
